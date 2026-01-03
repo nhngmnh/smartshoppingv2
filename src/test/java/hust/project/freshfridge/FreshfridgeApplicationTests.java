1bc2f0f0c848d91132c7001cf2d1ade6b6969f47
nhngmnh <nhungocminh2004@gmail.com>
2026-01-04 01:23:47 +0700

    test

--- src/test/java/hust/project/freshfridge/FreshfridgeApplicationTests.java
+++ src/test/java/hust/project/freshfridge/FreshfridgeApplicationTests.java
@@ -2,9 +2,18 @@ package hust.project.freshfridge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.ActiveProfiles;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableScheduling
 class FreshfridgeApplicationTests {
+
+	@MockBean
+	private FirebaseMessaging firebaseMessaging;
 
 	@Test
 	void contextLoads() {
